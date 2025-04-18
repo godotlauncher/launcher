fa86c1e01a7a51208cfc83556ab4e74c2713d8aa
Mario Debono <mario.debono@gmail.com>
2025-04-18 13:59:50 +0200

    feat: add setProjectWindowed function to manage project windowed state

--- src/electron/commands/projects.ts
+++ src/electron/commands/projects.ts
@@ -26,6 +26,14 @@ export async function getProjectsDetails(): Promise<ProjectDetails[]> {
     return projects;
 }
 
+export async function storeProjectsDetails(projects: ProjectDetails[]): Promise<ProjectDetails[]> {
+    const { configDir } = defaultDirs;
+    const projectListPath = path.resolve(configDir, PROJECTS_FILENAME);
+    const storedProjects = await storeProjectsList(projectListPath, projects);
+
+    return storedProjects;
+}
+
 export async function removeProject(project: ProjectDetails): Promise<ProjectDetails[]> {
     const { configDir } = defaultDirs;
     const projectListPath = path.resolve(configDir, PROJECTS_FILENAME);
@@ -89,12 +97,12 @@ export async function launchProject(project: ProjectDetails): Promise<void> {
     const currentMainWindow = getMainWindow();
 
     switch (prefs.post_launch_action) {
-    case 'minimize':
-        currentMainWindow?.minimize();
-        break;
-    case 'close_to_tray':
-        currentMainWindow?.close();
-        break;
+        case 'minimize':
+            currentMainWindow?.minimize();
+            break;
+        case 'close_to_tray':
+            currentMainWindow?.close();
+            break;
     }
 
     if (process.platform === 'linux') {
@@ -106,4 +114,21 @@ export async function launchProject(project: ProjectDetails): Promise<void> {
 
 export async function checkProjectIsValid(project: ProjectDetails): Promise<ProjectDetails> {
     return await checkProjectValid(project);
-} 
+}
+
+export async function setProjectWindowed(project: ProjectDetails, openWindowed: boolean): Promise<ProjectDetails> {
+
+    project.open_windowed = openWindowed;
+
+
+    const projects = await getProjectsDetails();
+    const projectIndex = projects.findIndex(p => p.path === project.path);
+
+    if (projectIndex !== -1) {
+        projects[projectIndex].open_windowed = openWindowed;
+        const updated = await storeProjectsDetails(projects);
+        ipcWebContentsSend('projects-updated', getMainWindow()?.webContents, updated);
+    }
+
+    return project;
+}
