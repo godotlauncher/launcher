58cd02df0105b52aa1480aeded078cc98097679f
Mario Debono <mario.debono@gmail.com>
2026-03-07 11:29:10 +0100

    chore: ensure test watch mode is disabled in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        watch: false,
         onConsoleLog(log, type) {
             const method = type === 'stderr' ? 'error' : 'log';
             console[method](log);
